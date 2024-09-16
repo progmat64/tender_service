95de7c80072e197111d8582a63a07f49f21d4c57
Alexandr6400 <admatveev17@gmail.com>
2024-09-16 21:55:47 +0300

    final version github

--- tender_service/tender_service/settings.py
+++ tender_service/tender_service/settings.py
@@ -1,19 +1,37 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
-# Загружаем переменные окружения из файла .env
-load_dotenv()
+# from dotenv import load_dotenv
 
+# # Загружаем переменные окружения из файла .env
+# load_dotenv()
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+# # Секретный ключ
+# SECRET_KEY = os.getenv("SECRET_KEY")
+
+# # Режим отладки
+# DEBUG = os.getenv("DEBUG", "False") == "True"
+
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Секретный ключ
-SECRET_KEY = os.getenv("SECRET_KEY")
 
-# Режим отладки
-DEBUG = os.getenv("DEBUG", "False") == "True"
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = (
+    "django-insecure-d!78(i@)xcu4eeb9bw+*mm(x5#5for^jtb9tf55&m)2lagl+hi"
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
@@ -58,24 +76,35 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "tender_service.wsgi.application"
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
+#         "ENGINE": "django.db.backends.postgresql",
+#         'NAME': "tenders",
+#         'USER': "adminDB",
+#         'PASSWORD': "12345",
+#         'HOST': "localhost"
 #     }
 # }
 
-# Настройка базы данных через переменные окружения
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('POSTGRES_DATABASE'),
-        'USER': os.getenv('POSTGRES_USERNAME'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
-    }
-}
+
+# # Настройка базы данных через переменные окружения
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.getenv('POSTGRES_DATABASE'),
+#         'USER': os.getenv('POSTGRES_USERNAME'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+#         'HOST': os.getenv('POSTGRES_HOST'),
+#         'PORT': os.getenv('POSTGRES_PORT'),
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
